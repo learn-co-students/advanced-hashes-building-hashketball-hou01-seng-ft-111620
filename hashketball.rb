3fb4eb5f0e841c5ca4517f67813e2cc52235aee1
Joy Evans <joyevans@me.com>
2020-10-19 21:31:49 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,23 @@
 # Write your code here!
+
+require 'pry'
+
+def game_hash
+  
+  hasketball = {
+    :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
+      :players => [
+        alan_anderson, reggie_evans, brook_lopez, mason_plumlee, jason_terry
+        ],
+    },
+    :away => {
+      :team_name => "Charlotte Hornets",
+      :colors => ["Turquoise", "Purple"],
+      :players => [
+        jeff_adrien, bismack_biyombo, desagna_diop, ben_gordon, kemba_walker
+        ]
+    }}
+  
+end
